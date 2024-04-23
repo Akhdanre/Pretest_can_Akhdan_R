65f5b8f124ed688078fb0c183288314420f0eaf4
Akhdanre <danakhdan12@gmail.com>
2024-04-23 23:16:05 +0700

    feat : create users migration

--- database/migrations/2024_04_23_155311_create_users_table.php
+++ database/migrations/2024_04_23_155311_create_users_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string("email", 100)->nullable(false)->unique();
+            $table->string("email", 100)->nullable(false)->unique("users_email_unique");
+            $table->string("password", 100)->nullable(false);
+            $table->string("no_ktp", 16)->nullable(false);
+            $table->string("name", 100)->nullable(false);
+            $table->date("birth_date")->nullable();
+            $table->string("gender", 20)->nullable();
+            $table->string("address", 100)->nullable();
+            $table->string("phone_number", 20)->nullable();
+            $table->string("profile_image", 100)->nullable();
+            $table->string("token", 100)->nullable()->unique("users_token_unique");
+            $table->timestamps();
         });
     }
 
