4e6d0dc439b90cd14ed162bbc9d26d1cd81bf936
Akhdanre <danakhdan12@gmail.com>
2024-04-24 11:11:07 +0700

    feat : create model relation

--- app/Models/relation.php
+++ app/Models/relation.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class relation extends Model
 {
-    use HasFactory;
+    protected $table = "tickets";
+    protected $primaryKey = "id";
+    protected $typeKey = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+
+    public function cityFrom() : HasOne {
+        return $this->hasOne(city::class, "from_id", "id");
+    }
+
+    public function cityDestination() : HasOne {
+        return $this->hasOne(city::class, "destination_id", "id");
+    }
 }
