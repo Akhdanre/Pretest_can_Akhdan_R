9c4569bb15edce31da33495cc6c70730edc1f98b
Akhdanre <danakhdan12@gmail.com>
2024-04-24 11:07:53 +0700

    feat : create role model

--- app/Models/role.php
+++ app/Models/role.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class role extends Model
-{
-    use HasFactory;
+class Role extends Model {
+    protected $table = "tickets";
+    protected $primaryKey = "id";
+    protected $typeKey = "int";
+    public $incrementing = true;
+
+    public function employess() : BelongsTo{
+        return $this->belongsTo(employee::class, "role_id", "id");
+    }
 }
