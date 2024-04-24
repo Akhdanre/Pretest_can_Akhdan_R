83be57aae3cf04ebffe73989cf775227022923c3
Akhdanre <danakhdan12@gmail.com>
2024-04-24 12:58:39 +0700

    feat : create register'

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'no_ktp' => $this->no_ktp,
+            'birth_date' => $this->birth_date,
+            'gender' => $this->gender,
+            'address' => $this->address,
+            'phone_number' => $this->phone_number,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
