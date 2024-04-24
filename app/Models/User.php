e6ba50d64d8ce5ffd5cc39d3df4fd5cad3be15de
Akhdanre <danakhdan12@gmail.com>
2024-04-24 10:51:38 +0700

    feat : create model users and ticket

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use PHPUnit\Framework\Attributes\Ticket;
 
-class User extends Model
-{
-    use HasFactory;
+class User extends Model {
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+
+    public function tickets(): HasMany {
+        return $this->hasMany(Ticket::class, "user_id", "id");
+    }
 }
--- app/Models/ticket.php
+++ app/Models/ticket.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class ticket extends Model
-{
-    use HasFactory;
+class ticket extends Model {
+    protected $table = "tickets";
+    protected $primaryKey = "id";
+    protected $typeKey = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+
+
+    public function user(): BelongsTo{
+        return $this->belongTo(User::class, "id", "user_id");
+    }
 }
